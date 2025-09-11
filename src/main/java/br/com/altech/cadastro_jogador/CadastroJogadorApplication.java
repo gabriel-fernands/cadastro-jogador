9830cf838fb9079807bbea6d981d8210432fda04
gabriel <mendesgabrielfernades@gmail.com>
2025-09-10 22:46:52 -0300

    first-commit

---
+++ src/main/java/br/com/altech/cadastro_jogador/CadastroJogadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.altech.cadastro_jogador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroJogadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroJogadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/altech/cadastro_jogador/CadastroJogadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.altech.cadastro_jogador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroJogadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
